cc5f9150d763c6652cf2a22173c6927d1f156967
Sai Ke WANG <sai@onceking.com>
2015-05-29 01:04:25 -0400

    randomize leaky objects

--- leak.js
+++ leak.js
@@ -1,6 +1,6 @@
 function op(arr, n){
     for(var i=0; i<n; ++i){
-	var d = new Date();
+	var d = new Date(Date.now() + Math.random()*100);
 	arr.push(d);
 	arr.push(d + '');
 	arr.push(d.getTime());
